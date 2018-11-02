d47590ac15f416c2edac8ba22bc78469f775a219
Matt Lacey <mlacey015@gmail.com>
2018-11-01 21:18:55 -0700

    cleaned corrupted package-lock.json and installed normalize.css

---
+++ src/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Header extends React.Component {
+	render() {
+		return (
+			<header className="mainHeader">
+				<h2>Choresy</h2>
+			</header>
+		);
+	}
+}
+
+export default Header;
